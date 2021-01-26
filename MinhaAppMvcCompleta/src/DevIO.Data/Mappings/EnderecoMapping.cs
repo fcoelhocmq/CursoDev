0cace6a18f4e3f9cfeea7018a2e425775a8db11a
Dev1 <Dev1@DESKTOP-ILP1AAV>
2021-01-25 23:20:19 -0300

    ok

---
+++ MinhaAppMvcCompleta/src/DevIO.Data/Mappings/EnderecoMapping.cs
@@ -0,0 +1,49 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevIO.Data.Mappings
+{
+    public class EnderecoMapping : IEntityTypeConfiguration<Endereco>
+    {
+        public void Configure(EntityTypeBuilder<Endereco> builder)
+        {
+            builder.HasKey(c => c.Id);
+           
+            builder.Property(c=> c.Logradouro)
+                .IsRequired()
+                .HasColumnType("nvarchar(200)");
+
+            builder.Property(c => c.Numero)
+                 .IsRequired()
+                 .HasColumnType("nvarchar(50)");
+
+            builder.Property(c => c.Cep)
+                 .IsRequired()
+                 .HasColumnType("nvarchar(8)");
+
+            builder.Property(c => c.Complemento)
+                .IsRequired()
+                .HasColumnType("nvarchar(250)");
+
+            builder.Property(c => c.Bairro)
+                 .IsRequired()
+                 .HasColumnType("nvarchar(100)");
+           
+            builder.Property(c => c.Cidade)
+                 .IsRequired()
+                 .HasColumnType("nvarchar(100)");
+            builder.Property(c => c.Estado)
+                 .IsRequired()
+                 .HasColumnType("nvarchar(50)");
+
+
+
+            builder.ToTable("Enderecos");
+
+        }
+    }
+}
